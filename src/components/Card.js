172d27207d8bcca054c00f2d5f9cb64b606c289d
VictoriaStudios <88744359+VictoriaStudios@users.noreply.github.com>
2021-12-16 20:06:05 -0500

    Added buttons to navigate cards and fade animations

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,22 @@
 
 
-const Card = ({title, text, image}) => {
+const Card = ({ title, text, image, incrementIndex, decrementIndex }) => {
+    function handleNext(){
+        incrementIndex()
+
+    }
+
+    function handlePrev(){
+        decrementIndex()
+    }
     return (
         <div>
             <h1>{title}</h1>
-            <image src={image}/>
+            <div className="cardSlider">
+                <h3 onClick={handlePrev}> &lt; </h3>
+                <img className="cardImage fadeOut" src={image} />
+                <h3 onClick={handleNext}> &gt; </h3>
+            </div>
             <h2>{text}</h2>
         </div>
     )
--- src/components/CardViewer.js
+++ src/components/CardViewer.js
@@ -1,17 +1,45 @@
 import { useState } from "react"
 import Card from "./Card"
+import image1 from "../images/image1.jpg"
 
 const CardViewer = () => {
-    const [cards, setCards] =  useState([]) 
-    const [currentCard, setCurrentCard] = useState ('')
-    const testCard = {title:"Test title",
-                        image:"",
-                        text:"Test text"
-                    }
+    const card1 = {
+        title: "Hola mi amor",
+        image: image1,
+        text: "Este es un regalito para tí"
+    }
+
+    const card2 = {
+        title: "Test title2",
+        image: "",
+        text: "Test text2"
+    }
+    const [cards, setCards] = useState([card1, card2])
+    const [cardIndex, setCardIndex] = useState(0)
+
+    function incrementIndex () {
+        if (cardIndex < cards.length-1){
+            setCardIndex(cardIndex+1)
+        }
+        else{
+            setCardIndex(0)
+        }
+    }
+
+    function decrementIndex() {
+        if (cardIndex === 0) {
+            setCardIndex(cards.length-1)
+        }
+        else{
+            setCardIndex(cardIndex-1)
+        }
+
+    }
+
     return (
         <div>
             <div className="cardBackground">
-                <Card title={testCard.title} image={testCard.image} text={testCard.text}/>
+                <Card title={cards[cardIndex].title} text={cards[cardIndex].text} image={cards[cardIndex].image} incrementIndex={incrementIndex} decrementIndex={decrementIndex} />
             </div>
         </div>
     )
